48fc4c7b5b6408925e351c5afb5952f195f2adee
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2023-07-05 19:46:00 +0200

    Svelte Sign In Page
    Sign In Page using Svelte

---
+++ frontend/my-svelte-app/src/App.svelte
@@ -0,0 +1,92 @@
+<script>
+	export let name;
+	import Image from "./Image.svelte"
+	import Form from "./Form.svelte"
+	import SignIn from "./Sign-In.svelte";
+</script>
+
+<main>
+	<!-- <h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+	<div class="body">
+		<div class="logo">
+			<h1>Fair Chance</h1>
+			<Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/SAPS_badge.svg/800px-SAPS_badge.svg.png" alt="badge"/>
+			<h2>Where justice meets trust, security and transparency</h2>
+		</div>
+		<div class="sign-in">
+			<h1 id="sign">Sign In</h1>
+			<h2 id="catch">To serve South Africans truthfully</h2>
+			<Form></Form>
+		</div>	
+	</div>
+	
+</main>
+
+<style>
+	.logo{
+		margin: 15vh 0vh 5vh 25vh;
+		background-color: #091D30 ;
+		width: 90vh;
+		height: 70vh;
+		text-align: center;
+		
+	}
+
+	.sign-in{
+		margin-top: 15vh;
+		background-color: white;
+		width: 59vh;
+		height: 70vh;
+	}
+
+	.body{
+		display: flex;
+		background-color: #4A5158 ;
+		height: 98vh;
+		align-content: center;
+	}
+	main {
+		background-color: #4A5158;
+		viewport-fit: 0px;
+	}
+
+	#sign{
+		color: black;
+		text-transform: none;
+		font-size: 3em;
+		margin-left: 5vh;
+		margin-top: 3vh;
+		font-weight: 500;
+		font-family: 'Calibri';
+		margin-bottom: 0px;
+	}
+
+	h1 {
+		margin-top: 3vh;
+		color: white;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 500;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+
+	h2{
+		color: white;
+		font-size: 1.2em;
+	}
+
+	#catch{
+		color:#091D30;
+		font-weight: 400;
+		font-size: 1em;
+		margin-left: 5vh;
+		margin-top: 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ frontend/my-svelte-app/src/Form.svelte
@@ -0,0 +1,59 @@
+<form onsubmit="saveData(event)">
+    <input type="text" id="name" name="name" required placeholder="Username"><br><br>
+    <input type="password" id="pass" name="password" required placeholder="Password"><br><br>
+    <a href="https://www.youtube.com/watch?v=gdZLi9oWNZg&pp=ygUDYnRz"><u><h2>I forgot my password</h2></u></a>
+    <input type="submit" id="submit" value="Sign In">
+</form>
+
+
+<style>
+
+    #submit{
+        background-color: #091D30;
+        color: white;
+        margin-top: 5vh;
+        text-align: center;
+        margin-left: 18vh;
+        width: 25vh;
+        height: 6vh;
+        border-radius: 10px;
+    }
+
+    button:hover{
+        background-color: #fffb0362;
+    }
+
+    #name{
+        margin-top: 5vh;
+        padding-left: 2vh;
+        padding-top: 2vh;
+        padding-bottom: 2vh;
+        margin-left: 5vh;
+        background-color: rgb(207, 207, 207);
+        height: 6vh;
+        width: 50vh;
+        border-radius: 10px;
+    }
+
+    #pass{
+        padding-left: 2vh;
+        padding-top: 2vh;
+        padding-bottom: 2vh;
+        margin-top: 2vh;
+        margin-left: 5vh;
+        background-color:  rgb(207, 207, 207);
+        height:6vh;
+        width: 50vh;
+        border-radius: 10px;
+    }
+
+    h2{
+		color:#091D30;
+		font-weight: 400;
+		font-size: 1em;
+		margin-left: 5vh;
+		margin-top: 0;
+        font-weight: 700;
+        
+	}
+</style>
