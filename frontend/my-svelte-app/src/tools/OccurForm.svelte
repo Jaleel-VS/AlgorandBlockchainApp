c97d34726b69eb4636c4af7d64d5585a52316882
Jaleel-VS <jeremikress@gmail.com>
2023-07-08 10:55:24 +0200

    svelte occurrence form update

--- frontend/my-svelte-app/src/tools/OccurForm.svelte
+++ frontend/my-svelte-app/src/tools/OccurForm.svelte
@@ -2,22 +2,84 @@
     let name =""
     let surname =""
     let id =""
+    let cellnum=""
+    let homephone=""
+    let email=""
+    let address_1=""
+    let address_2=""
+    let desc=""
+    let officer_obs=""
+
+    /* FAST API MODEL
+    class Occurrence(BaseModel):
+    #occID: str
+    victim_name: str
+    victim_surname: str
+    victim_ID: str # Their id number 
+    cellphone: str
+    telephone: str
+    email: str
+    residential_address: str
+    occurance_description: str 
+    observations_other_info: str
+    */
+
+    const saveData = async() => {
+        const datadic = {
+            victim_name: name,
+            victim_surname: surname,
+            victim_ID: id,
+            cellphone: cellnum,
+            telephone: homephone,
+            email: email,
+            residential_address: address_1 + " " + address_2,
+            occurance_description: desc,
+            observations_other_info: officer_obs
+        }
+
+        const backendURL = "http://127.0.0.1:8000/";
+
+        try {
+            const response = await fetch(`${backendURL}occurrence`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(datadic),
+            });
+            const data = await response.json();
+            console.log(data);
+
+            if (data.success) {
+                console.log("Occurrence successfully logged");
+            }
+            else{
+                console.log(data.message)
+            }
+        
+
+            console.log("Data sent to the backend successfully");
+        } catch (error) {
+            console.error("Error sending data to the backend:", error);
+        }
+    }
 </script>
-    
 
 <form on:submit={saveData}>
-    
     <div class="names">
-        <label>Name:
+        <label
+            >Name:
             <input
                 bind:value={name}
                 type="text"
                 id="name"
                 name="name"
                 required
-                placeholder="e.g. Mmabatho"/>
+                placeholder="e.g. Mmabatho"
+            />
         </label>
-        <label> Surname:
+        <label>
+            Surname:
             <input
                 bind:value={surname}
                 type="text"
@@ -29,54 +91,62 @@
         </label>
     </div>
 
-    <label>Identification Number:
+    <label
+        >Identification Number:
         <input
             bind:value={id}
             type="text"
             id="idNum"
             name="idNum"
             required
-            placeholder="e.g. 0123456789123"/>
+            placeholder="e.g. 0123456789123"
+        />
     </label>
 
     <h2>Contact Details</h2>
 
     <div class="phones">
-        <label>Cell:
+        <label
+            >Cell:
             <input
-                bind:value={id}
+                bind:value={cellnum}
                 type="phone"
                 id="cell"
                 name="cell"
                 required
-                placeholder="e.g. (+27)78 256 7765"/>
+                placeholder="e.g. (+27)78 256 7765"
+            />
         </label>
 
-        <label>Home Phone:
+        <label
+            >Home Phone:
             <input
-                bind:value={id}
+                bind:value={homephone}
                 type="phone"
                 id="cell"
                 name="cell"
                 required
-                placeholder="e.g. (+21)78 256 7765"/>
-        </label>   
+                placeholder="e.g. (+21)78 256 7765"
+            />
+        </label>
     </div>
 
-    <label>E-mail address:
+    <label
+        >E-mail address:
         <input
-            bind:value={id}
+            bind:value={email}
             type="email"
             id="email"
             name="email"
             required
-            placeholder="e.g. queen.charlotte@gmail.com"/>
+            placeholder="e.g. queen.charlotte@gmail.com"
+        />
     </label>
 
     <h2>Residential Address</h2>
 
     <input
-        bind:value={id}
+        bind:value={address_1}
         type="text"
         id="address_1"
         name="address"
@@ -84,38 +154,34 @@
     /><br /><br />
 
     <input
-        bind:value={id}
+        bind:value={address_2}
         type="text"
         id="address_2"
         name="address"
-        required/>
+        required
+    />
 
     <h1 id="v">Additional Information</h1>
 
     <h2>Occurrence Desciption</h2>
 
-        <input
-            bind:value={id}
-            type="textarea"
-            id="desc"
-            name="desc"
-            required/>
-    
+    <input bind:value={desc} type="textarea" id="desc" name="desc" required />
+
     <h2>Officer Observation/Any other information</h2>
 
-        <input
-            bind:value={id}
-            type="textarea"
-            id="desc"
-            name="desc"
-            required/><br /><br />
+    <input
+        bind:value={officer_obs}
+        type="textarea"
+        id="desc"
+        name="desc"
+        required
+    /><br /><br />
 
     <button>Submit Occurrence</button>
-
 </form>
 
 <style>
-    button{
+    button {
         background-color: #091d30;
         color: white;
         text-align: center;
@@ -125,8 +191,8 @@
         border-radius: 10px;
     }
 
-    #v{
-        color: #091D30;
+    #v {
+        color: #091d30;
         font-size: 30px;
         margin: auto;
         text-align: center;
@@ -134,7 +200,7 @@
         margin-bottom: 1vh;
     }
 
-    #desc{
+    #desc {
         padding-left: 0.6vh;
         padding-top: 0.6vh;
         padding-bottom: 0.6vh;
@@ -145,9 +211,9 @@
         border-radius: 10px;
         margin-bottom: 0%;
     }
-    
-    h2{
-        color: #091D30;
+
+    h2 {
+        color: #091d30;
         font-size: 15px;
         margin-left: 2vh;
     }
@@ -155,19 +221,19 @@
         display: flex;
     }
 
-    .phones{
+    .phones {
         display: flex;
         margin-bottom: 2vh;
     }
 
-    label{
+    label {
         font-size: 12px;
         margin-left: 2vh;
         font-weight: bold;
-        
     }
 
-    #address_1, #address_2 {
+    #address_1,
+    #address_2 {
         padding-left: 0.6vh;
         padding-top: 0.6vh;
         padding-bottom: 0.6vh;
@@ -178,8 +244,7 @@
         border-radius: 10px;
         margin-bottom: 0%;
     }
-    #cell{
-
+    #cell {
         padding-left: 0.6vh;
         padding-top: 0.6vh;
         padding-bottom: 0.6vh;
@@ -188,9 +253,9 @@
         height: 5vh;
         width: 22vh;
         border-radius: 10px;
-
     }
-    #idNum, #email{
+    #idNum,
+    #email {
         padding-left: 0.6vh;
         padding-top: 0.6vh;
         padding-bottom: 0.6vh;
@@ -200,7 +265,7 @@
         width: 44vh;
         border-radius: 10px;
     }
-    #name{
+    #name {
         /* margin-top: 5vh; */
         padding-left: 0.6vh;
         padding-top: 0.6vh;
