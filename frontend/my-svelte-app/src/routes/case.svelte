400e3ae9c02665ba160faf4265ae5394a1703700
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2023-07-09 23:59:04 +0200

    Current case docket with updated CSS
    Updated CSS

--- frontend/my-svelte-app/src/routes/case.svelte
+++ frontend/my-svelte-app/src/routes/case.svelte
@@ -93,63 +93,59 @@
                         />
                     </label>
 
-                        <label>
-                            Time of offense:
-                            <input
-                                type="time"
-                                id="offTime"
-                                name="offTime"
-                                bind:value={offTime}
-                                required
-                            />
-                        </label>
-                    </div>
-                </div>
+                    <label>
+                        Time of offense:
+                        <input
+                            type="time"
+                            id="offTime"
+                            name="offTime"
+                            bind:value={offTime}
+                            required
+                        />
+                    </label>
 
-                <div class="description">
-                    <div id="offense_description_div">
-                        <label>
-                            Nature and description of offense:
-                            <br />
-                            <textarea
-                                id="offdesc"
-                                name="offdesc"
-                                bind:value={offdesc}
-                                required
-                            />
-                        </label>
+                    <label>
+                        Nature and description of offense:
+                        <br />
+                        <textarea
+                            id="offdesc"
+                            name="offdesc"
+                            bind:value={offdesc}
+                            required
+                        />
+                    </label>
 
-                        <label>
-                            Crime Code:
-                            <br />
-                            <input
-                                type="text"
-                                id="offCode"
-                                name="offCode"
-                                bind:value={offCode}
-                                required
-                            />
-                        </label>
+                    <label>
+                        Crime Code:
+                        <br />
+                        <input
+                            type="text"
+                            id="offCode"
+                            name="offCode"
+                            bind:value={offCode}
+                            required
+                        />
+                    </label>
 
-                        <label>
-                            Nature of property damage/injuries:
-                            <br />
-                            <textarea
-                                id="offNature"
-                                name="offNature"
-                                bind:value={offNature}
-                                required
-                            />
-                        </label>
-                    </div>
+                    <label>
+                        Nature of property damage/injuries:
+                        <br />
+                        <textarea
+                            id="offNature"
+                            name="offNature"
+                            bind:value={offNature}
+                            required
+                        />
+                    </label>
+        
                 </div>
             </div>
 
             <h1>PART B</h1>
             <h2>ACCUSED DETAILS</h2>
 
-            <div class="body">
-                <div class="offense">
+            <div class="body2">
+                <div class="accusedDetails">
                     <p>Accused First Name:</p>
                     <div id="firstname_div">
                         <input type="text" name="firstname" id="firstname" bind:value={firstname} />
@@ -160,82 +156,113 @@
                         <input type="text" name="lastname" id="lastname" bind:value={lastname} />
                     </div>
 
-                    <p>Gender:</p>
-                    <div id="gender_div">
-                        <input id="male" type="radio" name="gender" bind:group={gender} value="Male" /> Male <br />
-                        <input id="female" type="radio" name="gender" bind:group={gender} value="Female" /> Female <br />
-                        <input id="non-binary" type="radio" name="gender" bind:group={gender} value="Non-Binary" /> Non-Binary <br />
-                    </div>
+                    <div class="gender_race">
+                        <div class="gender">
+                            <label>Gender:
+                                <br/>
+                                <br/>
+                                <input id="male" type="radio" name="gender" bind:group={gender} value="Male" /> Male <br />
+                                <input id="female" type="radio" name="gender" bind:group={gender} value="Female" /> Female <br />
+                                <input id="non-binary" type="radio" name="gender" bind:group={gender} value="Non-Binary" /> Non-Binary <br />    
+                            </label>
+                        </div>    
 
-                    <p>Race:</p>
-                    <div id="race_div">
-                        <input id="white" type="radio" name="race" bind:group={race} value="White" required /> White <br />
-                        <input id="african" type="radio" name="race" bind:group={race} value="African" /> African <br />
-                        <input id="asian" type="radio" name="race" bind:group={race} value="Asian" /> Asian <br />
-                        <input id="coloured" type="radio" name="race" bind:group={race} value="Coloured" /> Coloured <br />
-                    </div>
-                </div>
+                        <div class="race">
+                            <label>Race:
+                                <br/>
+                                <br/>
+                                <input id="african" type="radio" name="race" bind:group={race} value="African" /> African <br />
+                                <input id="asian" type="radio" name="race" bind:group={race} value="Asian" /> Asian <br />
+                                <input id="coloured" type="radio" name="race" bind:group={race} value="Coloured" /> Coloured <br />
+                                <input id="white" type="radio" name="race" bind:group={race} value="White" required /> White <br />
+                            </label>
+                        </div>
 
-                <div class="description">
+                    </div>
+                    
                     <div id="accused_description_div">
                         <label>
                             Accused Age:
                             <br />
                             <input
-                                type="textarea"
-                                id="offdesc"
-                                name="offdesc"
+                                type="number"
+                                id="accAge"
+                                name="accAge"
                                 required/>
-                    </label>
+                        </label>
                     
-                    <input type="submit" id="off_details" value="Submit Offense Details" />
-                </form>
+                        <input type="submit" id="off_details" value="Submit Offense Details" />
+                    </div>
+
+                </div>
+            
             </div>
 
-            <button type="submit" id="off_details">Submit Offense Details</button>
         </div>
-    </main>
-</form>
+</main>
 
 
 <style>
 
+
+#accAge{
+    height: 5vh;
+    width: 10vh;
+    margin-left: 5vh;
+    margin-top: 2vh;
+}
+#firstname, #lastname{
+    margin-left: 5vh;
+    width: 60vh;
+}
+
+.gender_race{
+    display: flex;
+    margin-top: 2vh;
+}
+
+.gender{
+    margin-right: 10vh;
+    margin-left: 5vh;
+}
 #off_details{
     background-color: #091d30;
     color: white;
     text-align: center;
-    margin-left: 83vh;
+    margin-left: 18vh;
     width: 35vh;
     height: 6vh;
     border-radius: 10px;
-    margin-top: 2vh ;
+    margin-top: 7vh ;
 }
 
-    #offCode {
-        margin-top: 2vh;
-        margin-left: 5vh;
-        width: 60vh;
-    }
-    #offdesc {
-        margin-top: 2vh;
-        margin-left: 5vh;
-        height: 10vh;
-        width: 60vh;
-    }
-    .description {
-        background-color: grey;
-        padding: 2vh;
-        color: white;
-        width: 70vh;
-        margin: 5vh 10vh 2vh 5vh;
-        height: 45vh;
-    }
+#offCode {
+    margin-top: 2vh;
+    margin-left: 5vh;
+    width: 60vh;
+}
+
+#offdesc {
+    margin-top: 2vh;
+    margin-left: 5vh;
+    height: 10vh;
+    width: 60vh;
+}
+
+#offNature {
+    margin-top: 2vh;
+    margin-left: 5vh;
+    height: 10vh;
+    width: 60vh;
+}
 
 p{
+    color: #091D30;
     font-weight: bold;
     margin-bottom: 1vh;
 }
 label{
+    color: #091D30;
     font-weight: bold;
 }
 .offense{
@@ -243,8 +270,25 @@ label{
     background-color:grey;
     color: white;
     width: 70vh;
-    margin:5vh 5vh 2vh 10vh;
-    height: 45vh;
+    margin:5vh 5vh 2vh 5.5vh;
+    height: 72vh;
+}
+
+.body2{
+    background-color: white;
+    width: 85vh;
+    height: 86vh;
+    margin: auto;
+    display: flex;
+}
+
+.accusedDetails{
+    padding: 2vh;
+    background-color:grey;
+    color: white;
+    width: 70vh;
+    margin:5vh 5vh 2vh 5.5vh;
+    height: 64vh;
 }
 
     h2 {
@@ -263,8 +307,8 @@ h1{
 }
 .body{
     background-color: white;
-    width: 180vh;
-    height: 60vh;
+    width: 85vh;
+    height: 86vh;
     margin: auto;
     display: flex;
 }
@@ -285,7 +329,7 @@ h1{
 
 .docket{
     background-color: #E8E8E8 ;
-	height: 113vh;
+	height: 240vh;
 	align-content: center;
 }
 
