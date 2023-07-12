dd1c46777aecd5803e920f0ae643395faa63902d
Jaleel-VS <jeremikress@gmail.com>
2023-07-12 18:46:46 +0200

    work on backend

--- backend/routes/occurence_route.py
+++ backend/routes/occurence_route.py
@@ -3,12 +3,12 @@ import boto3
 #import zipfile # Wanna make our dockets into a zip file
 from fastapi import APIRouter, UploadFile, HTTPException, status
 from models.occurrence import Occurrence
-from config.database import get_occ_count, update_occ_count
+from config.database import get_occ_count, update_occ_count, insert_occurrence
 from utils.helper import send_hash_to_blockchain, s3_resource, s3_upload
 from services.transaction import Transaction
 from services.utils import get_hash
 import json
-import pymongo
+from typing import Any
 
 occurrence_router = APIRouter()
 
@@ -20,14 +20,19 @@ occurrence_router = APIRouter()
 @occurrence_router.post("/log_occurrence") 
 async def create_occurrence(occurrence: Occurrence):
     try:
-        occurrence_bytes = json.dumps(occurrence.dict()).encode()
+        # occurrence_bytes = json.dumps(occurrence.dict()).encode()
         # Sending to Blockchain
-        response_dict = dict(send_hash_to_blockchain(occurrence_bytes))
+        # response_dict = dict(send_hash_to_blockchain(occurrence_bytes))
         # Uploading to s3 bucket
-        occurrence_number = get_occ_count()
-        await s3_upload(contents=occurrence_bytes, key=f"Occurrence_{occurrence_number}.txt", folder ="OCCURRENCES")
+        # occurrence_number = get_occ_count()
+        # await s3_upload(contents=occurrence_bytes, key=f"Occurrence_{occurrence_number}.txt", folder ="OCCURRENCES")
+        print(occurrence)
+        print(occurrence.dict())
+        insert_occurrence(occurrence.dict())
         update_occ_count()
-        return response_dict
+        return {
+            "success": True,
+        }
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
